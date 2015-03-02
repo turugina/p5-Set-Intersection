42b0aa2c16aad7b1724bad38ffe506081981f3db
turugina <turugina@floralcompany.jp>
2015-03-02 14:31:45 +0900

    for 0.04

--- lib/Set/Intersection.pm
+++ lib/Set/Intersection.pm
@@ -9,11 +9,11 @@ Set::Intersection - provides an API to get intersection (of set theory) of ARRAY
 
 =head1 VERSION
 
-Version 0.03;
+Version 0.04;
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
